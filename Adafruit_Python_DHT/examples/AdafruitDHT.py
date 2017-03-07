c8f2c5d00b9c02496c7d82f883bb2bb24565a012
Henry <henry@af-schneider.de>
2017-03-08 00:09:40 +0100

    insert seperator for output info

--- Adafruit_Python_DHT/examples/AdafruitDHT.py
+++ Adafruit_Python_DHT/examples/AdafruitDHT.py
@@ -48,7 +48,9 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 if humidity is not None and temperature is not None:
+    print('-----------------')
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('-----------------')
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
